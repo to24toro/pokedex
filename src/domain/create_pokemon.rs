a52c862777d1afd894c30dc56cc1b408e3d0b339
to24toro <to24toro@gmail.com>
2022-09-15 15:34:01 +0900

    In-memory repository

--- src/domain/create_pokemon.rs
+++ src/domain/create_pokemon.rs
@@ -1,4 +1,5 @@
 use crate::domain::entities::{PokemonName, PokemonNumber, PokemonTypes};
+use crate::repositories::pokemon::{InMemoryRepository, Repository, Insert};
 use std::convert::TryFrom;
 
 struct Request {
@@ -9,16 +10,22 @@ struct Request {
 
 enum Response {
     Ok(u16),
-    BadRequest
+    BadRequest,
+    Conflict,
+    Error,
 }
 
-fn execute(req: Request) -> Response {
+fn execute(repo: &mut dyn Repository, req: Request) -> Response {
     match (
         PokemonNumber::try_from(req.number),
         PokemonName::try_from(req.name),
         PokemonTypes::try_from(req.types),
     ) {
-        (Ok(number), Ok(_), Ok(_)) => Response::Ok(u16::from(number)),
+        (Ok(number), Ok(name), Ok(types)) => match repo.insert(number, name, types) {
+            Insert::Ok(number) =>Response::Ok(u16::from(number)),
+            Insert::Conflict => Response::Conflict,
+            Insert::Error => Response::Error,
+        },
         _ => Response::BadRequest,
     }
 }
@@ -29,6 +36,7 @@ mod test {
 
     #[test]
     fn it_should_return_the_pokemon_number_otherwise() {
+        let mut repo = InMemoryRepository::new();
         let number = 25;
         let req = Request {
             number,
@@ -36,27 +44,67 @@ mod test {
             types: vec![String::from("Electric")],
         };
 
-        let res = execute(req);
+        let res = execute(&mut repo, req);
 
         match res {
-            Response::Ok(res_number) => assert_eq!(res_number,number),
+            Response::Ok(res_number) => assert_eq!(res_number, number),
             _ => unreachable!(),
         }
     }
 
     #[test]
     fn it_should_return_a_bad_request_error_when_request_is_invalid() {
+        let mut repo = InMemoryRepository::new();
         let req = Request {
             number: 25,
             name: String::from(""),
             types: vec![String::from("Electric")],
         };
 
-        let res = execute(req);
+        let res = execute(&mut repo, req);
 
         match res {
-            Response::BadRequest => {},
+            Response::BadRequest => {}
             _ => unreachable!(),
         }
+    }
+
+    #[test]
+    fn it_should_return_a_conflict_error_when_pokemon_number_already_exists() {
+        let number = PokemonNumber::try_from(25).unwrap();
+        let name = PokemonName::try_from(String::from("Pikachu")).unwrap();
+        let types = PokemonTypes::try_from(vec![String::from("Electric")]).unwrap();
+        let mut repo = InMemoryRepository::new();
+        repo.insert(number,name,types);
+        let req = Request {
+            number: 25,
+            name: String::from("Charamander"),
+            types: vec![String::from("Fire")],
+        };
+
+        let res = execute(&mut repo, req);
+
+        match res {
+            Response::Conflict => {},
+            _ => unreachable!(),
+        }
+    }
+
+    #[test]
+    fn it_should_return_an_error_when_an_unexpected_error_happens() {
+        let mut repo = InMemoryRepository::new().with_error();
+        let number = 25;
+        let req = Request {
+            number,
+            name: String::from("Pikachu"),
+            types: vec![String::from("Electric")],
+        };
+
+        let res = execute(&mut repo, req);
+
+        match res {
+            Response::Error => {}
+            _ => unreachable!(),
+        };
     }
 }
--- src/domain/entities.rs
+++ src/domain/entities.rs
@@ -1,3 +1,6 @@
+use std::cmp::PartialEq;
+
+#[derive(PartialEq, Clone)]
 pub struct PokemonNumber(u16);
 
 impl TryFrom<u16> for PokemonNumber {
@@ -22,7 +25,7 @@ pub struct PokemonName(String);
 
 impl TryFrom<String> for PokemonName {
     type Error = ();
-    
+
     fn try_from(n: String) -> Result<Self, Self::Error> {
         if n.is_empty() {
             Err(())
@@ -36,7 +39,7 @@ pub struct PokemonTypes(Vec<PokemonType>);
 
 impl TryFrom<Vec<String>> for PokemonTypes {
     type Error = ();
-    
+
     fn try_from(ts: Vec<String>) -> Result<Self, Self::Error> {
         let mut pts = vec![];
 
@@ -52,15 +55,33 @@ impl TryFrom<Vec<String>> for PokemonTypes {
 
 enum PokemonType {
     Electric,
+    Fire,
 }
 
 impl TryFrom<String> for PokemonType {
     type Error = ();
-    
+
     fn try_from(t: String) -> Result<Self, Self::Error> {
         match t.as_str() {
             "Electric" => Ok(Self::Electric),
+            "Fire" => Ok(Self::Fire),
             _ => Err(()),
+        }
+    }
+}
+
+pub struct Pokemon {
+    pub number: PokemonNumber,
+    name: PokemonName,
+    types: PokemonTypes,
+}
+
+impl Pokemon {
+    pub fn new(number: PokemonNumber, name: PokemonName, types: PokemonTypes) -> Self {
+        Self {
+            number,
+            name,
+            types,
         }
     }
 }
