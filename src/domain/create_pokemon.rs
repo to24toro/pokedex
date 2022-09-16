210b6f9cf72d44aa76492bc222999398930c8de8
to24toro <to24toro@gmail.com>
2022-09-16 09:51:57 +0900

    http

--- src/domain/create_pokemon.rs
+++ src/domain/create_pokemon.rs
@@ -1,28 +1,29 @@
 use crate::domain::entities::{PokemonName, PokemonNumber, PokemonTypes};
-use crate::repositories::pokemon::{InMemoryRepository, Repository, Insert};
+use crate::repositories::pokemon::{InMemoryRepository, Insert, Repository};
 use std::convert::TryFrom;
+use std::sync::Arc;
 
-struct Request {
-    number: u16,
-    name: String,
-    types: Vec<String>,
+pub struct Request {
+    pub number: u16,
+    pub name: String,
+    pub types: Vec<String>,
 }
 
-enum Response {
+pub enum Response {
     Ok(u16),
     BadRequest,
     Conflict,
     Error,
 }
 
-fn execute(repo: &mut dyn Repository, req: Request) -> Response {
+pub fn execute(repo: Arc<dyn Repository>, req: Request) -> Response {
     match (
         PokemonNumber::try_from(req.number),
         PokemonName::try_from(req.name),
         PokemonTypes::try_from(req.types),
     ) {
         (Ok(number), Ok(name), Ok(types)) => match repo.insert(number, name, types) {
-            Insert::Ok(number) =>Response::Ok(u16::from(number)),
+            Insert::Ok(number) => Response::Ok(u16::from(number)),
             Insert::Conflict => Response::Conflict,
             Insert::Error => Response::Error,
         },
@@ -36,7 +37,7 @@ mod test {
 
     #[test]
     fn it_should_return_the_pokemon_number_otherwise() {
-        let mut repo = InMemoryRepository::new();
+        let mut repo = Arc::new(InMemoryRepository::new());
         let number = 25;
         let req = Request {
             number,
@@ -44,7 +45,7 @@ mod test {
             types: vec![String::from("Electric")],
         };
 
-        let res = execute(&mut repo, req);
+        let res = execute(repo, req);
 
         match res {
             Response::Ok(res_number) => assert_eq!(res_number, number),
@@ -54,14 +55,14 @@ mod test {
 
     #[test]
     fn it_should_return_a_bad_request_error_when_request_is_invalid() {
-        let mut repo = InMemoryRepository::new();
+        let mut repo = Arc::new(InMemoryRepository::new());
         let req = Request {
             number: 25,
             name: String::from(""),
             types: vec![String::from("Electric")],
         };
 
-        let res = execute(&mut repo, req);
+        let res = execute(repo, req);
 
         match res {
             Response::BadRequest => {}
@@ -74,25 +75,25 @@ mod test {
         let number = PokemonNumber::try_from(25).unwrap();
         let name = PokemonName::try_from(String::from("Pikachu")).unwrap();
         let types = PokemonTypes::try_from(vec![String::from("Electric")]).unwrap();
-        let mut repo = InMemoryRepository::new();
-        repo.insert(number,name,types);
+        let mut repo = Arc::new(InMemoryRepository::new());
+        repo.insert(number, name, types);
         let req = Request {
             number: 25,
             name: String::from("Charamander"),
             types: vec![String::from("Fire")],
         };
 
-        let res = execute(&mut repo, req);
+        let res = execute(repo, req);
 
         match res {
-            Response::Conflict => {},
+            Response::Conflict => {}
             _ => unreachable!(),
         }
     }
 
     #[test]
     fn it_should_return_an_error_when_an_unexpected_error_happens() {
-        let mut repo = InMemoryRepository::new().with_error();
+        let mut repo = Arc::new(InMemoryRepository::new().with_error());
         let number = 25;
         let req = Request {
             number,
@@ -100,7 +101,7 @@ mod test {
             types: vec![String::from("Electric")],
         };
 
-        let res = execute(&mut repo, req);
+        let res = execute(repo, req);
 
         match res {
             Response::Error => {}
--- src/repositories/pokemon.rs
+++ src/repositories/pokemon.rs
@@ -1,23 +1,24 @@
 use crate::domain::entities::{Pokemon, PokemonName, PokemonNumber, PokemonTypes};
+use std::sync::Mutex;
 
 pub enum Insert {
     Ok(PokemonNumber),
     Conflict,
     Error,
 }
-pub trait Repository {
-    fn insert(&mut self, number: PokemonNumber, name: PokemonName, types: PokemonTypes) -> Insert;
+pub trait Repository: Send + Sync {
+    fn insert(&self, number: PokemonNumber, name: PokemonName, types: PokemonTypes) -> Insert;
 }
 
 pub struct InMemoryRepository {
     error: bool,
-    pokemons: Vec<Pokemon>,
+    pokemons: Mutex<Vec<Pokemon>>,
 }
 
 impl InMemoryRepository {
-    pub fn new () -> Self {
-        let pokemons: Vec<Pokemon> = vec![];
-        Self { 
+    pub fn new() -> Self {
+        let pokemons: Mutex<Vec<Pokemon>> = Mutex::new(vec![]);
+        Self {
             error: false,
             pokemons,
         }
@@ -31,17 +32,24 @@ impl InMemoryRepository {
     }
 }
 
-
 impl Repository for InMemoryRepository {
-    fn insert(&mut self, number: PokemonNumber, name: PokemonName, types: PokemonTypes) -> Insert {
+    fn insert(&self, number: PokemonNumber, name: PokemonName, types: PokemonTypes) -> Insert {
         if self.error {
             return Insert::Error;
         }
-        if self.pokemons.iter().any(|pokemon| pokemon.number == number) {
+
+        let mut lock = match self.pokemons.lock() {
+            Ok(lock) => lock,
+            _ => return Insert::Error,
+        };
+
+        if lock.iter().any(|pokemon| pokemon.number == number) {
             return Insert::Conflict;
         }
+
         let number_clone = number.clone();
-        self.pokemons.push(Pokemon::new(number_clone, name, types));
+        lock.push(Pokemon::new(number_clone, name, types));
         Insert::Ok(number)
     }
 }
+
